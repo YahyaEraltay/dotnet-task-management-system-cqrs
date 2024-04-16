7488103098233dd060832b3c9593e708bff97130
Yahya Eraltay <eraltay35@gmail.com>
2024-04-16 17:51:22 +0300

    Department DTOs created

---
+++ TaskManagementSystem.Infrastructure/DTOs/DepartmentDTOs/DepartmentRequestModel/UpdateDepartmentRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Infrastructure.DTOs.DepartmentDTOs.DepartmentRequestModel
+{
+    public class UpdateDepartmentRequestDTO
+    {
+        public Guid Id { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ TaskManagementSystem.Infrastructure/DTOs/DepartmentDTOs/DepartmentResponseModel/DepartmentResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Infrastructure.DTOs.DepartmentDTOs.DepartmentResponseModel
+{
+    public class DepartmentResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
