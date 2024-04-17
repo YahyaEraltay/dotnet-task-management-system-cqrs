ce2fbf01846c4352de01263b974f0b1c3173c24e
Yahya Eraltay <eraltay35@gmail.com>
2024-04-17 09:29:37 +0300

    User DTOs created

---
+++ TaskManagementSystem.Infrastructure/DTOs/UserDTOs/UserRequestModel/CreateUserRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Infrastructure.DTOs.UserDTOs.UserRequestModel
+{
+    internal class UserDTO
+    {
+    }
+}
