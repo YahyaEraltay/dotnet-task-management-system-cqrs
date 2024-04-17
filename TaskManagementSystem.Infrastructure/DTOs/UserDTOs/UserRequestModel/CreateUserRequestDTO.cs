e88698aa1e307cb64925b321dd5c855291586fc8
Yahya Eraltay <eraltay35@gmail.com>
2024-04-17 10:13:50 +0300

    Some changes maded

--- TaskManagementSystem.Infrastructure/DTOs/UserDTOs/UserRequestModel/CreateUserRequestDTO.cs
+++ TaskManagementSystem.Infrastructure/DTOs/UserDTOs/UserRequestModel/CreateUserRequestDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TaskManagementSystem.Infrastructure.DTOs.UserDTOs.UserRequestModel
 {
-    internal class UserDTO
+    public class CreateUserRequestDTO
     {
     }
 }
