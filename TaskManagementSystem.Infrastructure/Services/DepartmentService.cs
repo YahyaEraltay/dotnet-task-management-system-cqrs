570a90dd1b9df8bce6ce4b71bb736402b9d84309
Yahya Eraltay <eraltay35@gmail.com>
2024-04-17 09:59:51 +0300

    Department service completed

---
+++ TaskManagementSystem.Infrastructure/Services/DepartmentService.cs
@@ -0,0 +1,123 @@
+﻿using TaskManagementSystem.Domain.Entites;
+using TaskManagementSystem.Infrastructure.DTOs.DepartmentDTOs.DepartmentRequestModel;
+using TaskManagementSystem.Infrastructure.DTOs.DepartmentDTOs.DepartmentResponseModel;
+using TaskManagementSystem.Infrastructure.Repositories;
+
+namespace TaskManagementSystem.Infrastructure.Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public async Task<List<DepartmentResponseDTO>> All()
+        {
+            var departments = await _departmentRepository.All();
+            var response = new List<DepartmentResponseDTO>();
+
+            if (departments != null)
+            {
+                foreach (var department in departments)
+                {
+                    response.Add(new DepartmentResponseDTO
+                    {
+                        Id = department.Id,
+                        DepartmentName = department.DepartmentName
+                    });
+                }
+                return response;
+            }
+            else
+            {
+                throw new Exception("There is no department");
+            }
+        }
+
+        public async Task<DepartmentResponseDTO> CreateDepartment(CreateDepartmentRequestDTO request)
+        {
+            var department = new Department()
+            {
+                DepartmentName = request.DepartmentName,
+            };
+
+            await _departmentRepository.Create(department);
+
+            var response = new DepartmentResponseDTO()
+            {
+                Id = department.Id,
+                DepartmentName = department.DepartmentName,
+            };
+
+            return response;
+        }
+
+        public async Task<DeleteDepartmentResponseDTO> DeleteDepartment(GetDepartmentRequestDTO request)
+        {
+            var department = await _departmentRepository.Detail(request.Id);
+            var response = new DeleteDepartmentResponseDTO();
+
+            if (department != null)
+            {
+                await _departmentRepository.Delete(department);
+
+                response.IsDeleted = true;
+                response.Message = "Department is deleted";
+            }
+            else
+            {
+                response.IsDeleted = false;
+                response.Message = "Department could not be deleted";
+            }
+
+            return response;
+        }
+
+        public async Task<DepartmentResponseDTO> Detail(GetDepartmentRequestDTO request)
+        {
+            var department = await _departmentRepository.Detail(request.Id);
+            var response = new DepartmentResponseDTO();
+
+            if (department != null)
+            {
+                response = new DepartmentResponseDTO()
+                {
+                    Id = department.Id,
+                    DepartmentName = department.DepartmentName,
+                };
+                return response;
+            }
+            else
+            {
+                throw new Exception("Department could not be find");
+            }
+        }
+
+        public async Task<DepartmentResponseDTO> UpdateDepartment(UpdateDepartmentRequestDTO request)
+        {
+            var department = await _departmentRepository.Detail(request.Id);
+            var response = new DepartmentResponseDTO();
+
+            if (department != null)
+            {
+                department.DepartmentName = request.DepartmentName;
+                await _departmentRepository.Update(department);
+
+                response = new DepartmentResponseDTO()
+                {
+                    Id = department.Id,
+                    DepartmentName = department.DepartmentName,
+                };
+
+                return response;
+            }
+            else
+            {
+                throw new Exception("Department could not be updated");
+            }
+        }
+    }
+}
---
+++ TaskManagementSystem.Infrastructure/Services/IDepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementSystem.Infrastructure.DTOs.DepartmentDTOs.DepartmentRequestModel;
+using TaskManagementSystem.Infrastructure.DTOs.DepartmentDTOs.DepartmentResponseModel;
+
+namespace TaskManagementSystem.Infrastructure.Services
+{
+    public interface IDepartmentService
+    {
+        Task<DepartmentResponseDTO> CreateDepartment(CreateDepartmentRequestDTO request);
+        Task<DepartmentResponseDTO> UpdateDepartment(UpdateDepartmentRequestDTO request);
+        Task<DeleteDepartmentResponseDTO> DeleteDepartment(GetDepartmentRequestDTO request);
+        Task<DepartmentResponseDTO> Detail(GetDepartmentRequestDTO request);
+        Task<List<DepartmentResponseDTO>> All();
+    }
+}
