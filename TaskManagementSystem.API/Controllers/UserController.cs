e9cb17caf29f3e25679f760a5eecce76f7d66799
Yahya Eraltay <eraltay35@gmail.com>
2024-04-24 10:31:45 +0300

    User controller created

---
+++ TaskManagementSystem.API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Application.Users.Commands.Create;
+using TaskManagementSystem.Application.Users.Commands.Delete;
+
+namespace Task_Management_System_CQRS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> Create(CreateUserRequest request)
+        {
+            var result = await _mediator.Send(request); 
+            return Ok(result);
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<ActionResult> Delete(DeleteUserRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
