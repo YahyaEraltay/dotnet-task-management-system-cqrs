16cca884d254e423007b964179a7418018a79a5a
Yahya Eraltay <eraltay35@gmail.com>
2024-04-24 15:25:00 +0300

    User controller completed

--- TaskManagementSystem.API/Controllers/UserController.cs
+++ TaskManagementSystem.API/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaskManagementSystem.Application.Users.Commands.Create;
 using TaskManagementSystem.Application.Users.Commands.Delete;
+using TaskManagementSystem.Application.Users.Commands.Update;
+using TaskManagementSystem.Application.Users.Queries.All;
+using TaskManagementSystem.Application.Users.Queries.Detail;
 
 namespace Task_Management_System_CQRS.Controllers
 {
@@ -20,12 +22,35 @@ namespace Task_Management_System_CQRS.Controllers
         [HttpPost("[action]")]
         public async Task<ActionResult> Create(CreateUserRequest request)
         {
-            var result = await _mediator.Send(request); 
+            var result = await _mediator.Send(request);
             return Ok(result);
         }
 
         [HttpDelete("[action]")]
         public async Task<ActionResult> Delete(DeleteUserRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult> Update(UpdateUserRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> All()
+        {
+            var request = new AllUserRequest();
+            var result = await _mediator.Send(request);
+                
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> Detail(DetailUserRequest request)
         {
             var result = await _mediator.Send(request);
             return Ok(result);
