9782cefdd9962ea3258f9eced0cfe90ea8de8c96
Yahya Eraltay <eraltay35@gmail.com>
2024-04-25 16:40:43 +0300

    To do task controller completed

---
+++ TaskManagementSystem.API/Controllers/ToDoTaskController.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Application.ToDoTasks.Commands.Create;
+using TaskManagementSystem.Application.ToDoTasks.Commands.Delete;
+using TaskManagementSystem.Application.ToDoTasks.Commands.Update;
+using TaskManagementSystem.Application.ToDoTasks.Queries.All;
+using TaskManagementSystem.Application.ToDoTasks.Queries.Detail;
+
+namespace Task_Management_System_CQRS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoTaskController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ToDoTaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> Create(CreateToDoTaskRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<ActionResult> Delete(DeleteToDoTaskRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult> Update(UpdateToDoTaskRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> All()
+        {
+            var request = new AllToDoTaskRequest();
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> Detail(DetailToDoTaskRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
