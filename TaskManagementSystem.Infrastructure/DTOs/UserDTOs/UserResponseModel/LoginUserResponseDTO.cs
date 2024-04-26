3d6fb5d1c4f81b5608748719a78a9dbc912a389f
Yahya Eraltay <eraltay35@gmail.com>
2024-04-26 17:18:13 +0300

    Login DTOs

---
+++ TaskManagementSystem.Infrastructure/DTOs/UserDTOs/UserResponseModel/LoginUserResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Infrastructure.DTOs.UserDTOs.UserResponseModel
+{
+    public class LoginUserResponseDTO
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
