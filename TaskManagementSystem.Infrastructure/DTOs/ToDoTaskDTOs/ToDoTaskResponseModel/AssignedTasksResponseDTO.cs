1bea3fcbcdc94e635d26d6e575419a7173509f21
Yahya Eraltay <eraltay35@gmail.com>
2024-04-30 16:32:27 +0300

    Assigned tasks response dto created

---
+++ TaskManagementSystem.Infrastructure/DTOs/ToDoTaskDTOs/ToDoTaskResponseModel/AssignedTasksResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TaskManagementSystem.Domain.Entites.ToDoTask;
+
+namespace TaskManagementSystem.Infrastructure.DTOs.ToDoTaskDTOs.ToDoTaskResponseModel
+{
+    public class AssignedTasksResponseDTO
+    {
+        public string CreatorUserName { get; set; }
+        public string AssignedUserName { get; set; }
+        public string AssignedDepartmentName { get; set; }
+        public string ToDoTaskName { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
